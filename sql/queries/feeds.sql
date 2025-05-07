61719bebb61f4ba85f58643623d59c72c1cbc4e0
SamW94 <sam.wild.1994@gmail.com>
2025-05-07 15:31:21 +0100

    feat: add GetNextFeedToFetch query and update functions in database library

--- sql/queries/feeds.sql
+++ sql/queries/feeds.sql
@@ -26,3 +26,9 @@ UPDATE feeds
 SET last_fetched_at = $1, updated_at = $1
 WHERE id = $2
 RETURNING *;
+
+-- name: GetNextFeedToFetch :one
+SELECT * 
+FROM feeds
+ORDER BY last_fetched_at NULLS FIRST
+LIMIT 1;
